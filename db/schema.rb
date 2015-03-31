263c9e1de64175d00abdd468be104519972166a3
Nathan Carter <nc8510@mcla.edu>
2015-03-31 11:42:36 -0400

    Added image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326143522) do
+ActiveRecord::Schema.define(version: 20150331151454) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "courses_students", id: false, force: :cascade do |t|
